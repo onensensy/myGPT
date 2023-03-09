8f22378ec09c52d1418f1a9b7eb2234d1806847c
Onen Sensy <sensyonen@gmail.com>
2023-03-10 01:30:00 +0300

    FEAT: Updated the ChatController - Added code to make the chat controller handle the new UI

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -13,7 +13,7 @@ class ChatController extends Controller
         if ($request->prompt == null) {
             return;
         }
-
+        $option = $request->option;
         $prompt = $request->prompt;
 
         // $client = OpenAI::client(env('OPENAI_API_KEY'));
@@ -34,24 +34,45 @@ class ChatController extends Controller
         //     'prompt' => sprintf('Write article about: %s', $title),
         // ]);
 
-        // $result = OpenAI::completions()->create([
-        //     'model' => 'text-davinci-003',
-        //     'prompt' => 'PHP is',
-        // ]);
-        $result = OpenAI::completions()->create([
-            "model" => "text-davinci-003",
-            "temperature" => 0.7,
-            "top_p" => 1,
-            "frequency_penalty" => 0,
-            "presence_penalty" => 0,
-            'max_tokens' => 600,
-            'prompt' => sprintf($prompt),
-        ]);
+        switch ($option) {
+            case 0:
+                $result = OpenAI::completions()->create([
+                    "model" => "text-davinci-003",
+                    "temperature" => 0.7,
+                    "top_p" => 1,
+                    "frequency_penalty" => 0,
+                    "presence_penalty" => 0,
+                    'max_tokens' => 600,
+                    'prompt' => sprintf($prompt),
+                ]);
+                $content = trim($result['choices'][0]['text']);
+                return view('chat', compact('prompt', 'content'));
 
-        // echo $result['choices'][0]['text']; // an open-source, widely-used, server-side scripting language.
 
-        $content = trim($result['choices'][0]['text']);
-        // dd($content);
-        return view('chat', compact('prompt', 'content'));
+                break;
+            case 1:
+                $result = OpenAI::completions()->create([
+                    "model" => "text-davinci-003",
+                    "temperature" => 0.7,
+                    "top_p" => 1,
+                    "frequency_penalty" => 0,
+                    "presence_penalty" => 0,
+                    'max_tokens' => 600,
+                    'prompt' => 'Laravel is a powerful PHP web application framework that provides a simple, elegant syntax for creating robust web applications. As a Laravel-focused AI model, my responses will be tailored to Laravel-specific topics, such as working with controllers, models, views, routes, middleware, and more. Let\'s dive into the world of Laravel programming!
+                ' . sprintf($prompt),
+                ]);
+                $content = trim($result['choices'][0]['text']);
+                return view('chat', compact('prompt', 'content'));
+
+
+                break;
+
+            default:
+                $content = "Help Module for " . $option . " has not yet been implemented. You can contribute to the code via
+
+https://github.com/onensensy/myGPT";
+                return view('chat', compact('prompt', 'content'));
+                break;
+        }
     }
 }
