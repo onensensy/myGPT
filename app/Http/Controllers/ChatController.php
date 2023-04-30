4368b516ccad68b816da8bc6fb1d47c8d489e44e
Onen Sensy <sensyonen@gmail.com>
2023-05-01 00:24:37 +0300

    Update ChatController.php
    Added comment on the index function in the chatcontroller

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class ChatController extends Controller
 {
+    /**
+     * This  Returns the chat page
+     * @return void
+     */
     public function index()
     {
         return view('chat');
